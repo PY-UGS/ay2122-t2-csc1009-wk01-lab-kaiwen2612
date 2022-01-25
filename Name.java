7fa65a371cb23b517c2d5a936cf6dd94df5039a0
kaiwen2612 <chang_kaiwen_648@hotmail.com>
2022-01-25 17:49:14 +0800

    Add files via upload
    Qn 2a -- Name.java
    Qn 2b -- Module.java
    Qn 2c -- Test.java

---
+++ Name.java
@@ -0,0 +1,6 @@
+public class Name {
+    public static void main (String[] args) {
+        String name = "Kai Wen";
+        System.out.println("Hello, I am " + name + " !");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main (String[] args) {
+        for (int x = 102; x > 66; x = x - 2) {
+            System.out.print("value of x is: " + (x-1));
+            System.out.print("\n");
+        }
+    }
+}
